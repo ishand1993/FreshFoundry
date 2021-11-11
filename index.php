6411debaba36e583996157531a80eefb22c7a69c
ishand1993 <79570930+ishand1993@users.noreply.github.com>
2021-11-10 18:32:43 -0600

    landing page
    Main file

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fresh Foundry</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class='main'>
+        <header>
+            <p>Welcome to <span class="logo-font">Fresh Foundry</span></p>
+        </header>
+        <div class="content">
+            <section>
+                <p>
+                    Get your fresh groceries and more, online.
+                </p>
+            </section>
+        </div>
+        <footer>
+            <p>&copy; 2021</p>
+        </footer>
+    </div>
+</body>
+</html>
